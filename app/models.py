9b5fccc2c15bb5149a576120b4f5fb277b5c2a5d
prism <git.pyprism@gmail.com>
2014-11-29 23:23:08 +0600

    updated model

--- app/models.py
+++ app/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, blank=True, null=True)
     content = models.TextField()
     added_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
     notify = models.DateField()
 
     def __str__(self):
@@ -15,3 +16,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return '%s %s' % (self.title, self.content)
